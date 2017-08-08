9ffa2ab5766175ec41ca3ac0b783fcb70b3c5e2a
wangqingqing <1129865033@qq.com>
2017-08-08 11:30:54 +0800

    当前状态

--- src/config/routes.js
+++ src/config/routes.js
@@ -11,7 +11,7 @@ import Me from '../Me.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  { name: 'Home', path: '/home', component: Home },
+  { name: 'Home', path: '/', component: Home },
   { name: 'Categories', path: '/categories', component: Category },
   { name: 'Shoppingcart', path: '/shopping-cart', component: Shoppingcart },
   { name: 'Me', path: '/me', component: Me }
@@ -22,6 +22,6 @@ export default new VueRouter({
   mode: 'history',
   base: __dirname,
   routes: routes,
-  linkActiveClass: 'active',
+  linkExactActiveClass: 'active',
   history: true
 })
