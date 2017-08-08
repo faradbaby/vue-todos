58e6d762aa8f9d7d4b8aa66b5b871d9455c773ed
wangqingqing <1129865033@qq.com>
2017-08-08 11:59:06 +0800

    新增swiper插件

--- src/Home.vue
+++ src/Home.vue
@@ -1,12 +1,37 @@
 <template>
-<div class="">
-  首页
+<div>
+  <div class="section">
+    <div class="">
+      <!-- 热门推荐 -->
+      <div class="swiper-container" ref="slider">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">Slide 1</div>
+            <div class="swiper-slide">Slide 2</div>
+            <div class="swiper-slide">Slide 3</div>
+        </div>
+        <!-- Add Pagination -->
+        <div class="swiper-pagination" ref="pagination"></div>
+    </div>
+      <!-- 快讯 -->
+    </div>
+  </div>
+  <div class="section">
+    <div class="">
+      <!-- 新书上架 -->
+    </div>
+  </div>
+  <div class="section">
+    <div class="">
+      <!-- 编辑推荐 -->
+    </div>
+  </div>
 </div>
 </template>
 <style>
 
 </style>
 <script>
+
 export default {
 
 }
